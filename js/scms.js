2926ae37308dd98ac3d851037dd6e7940f702558
M. C. DeMarco <mcd@mcdemarco.net>
2015-05-01 11:06:46 -0400

    Add history API.

--- js/scms.js
+++ js/scms.js
@@ -8,8 +8,7 @@ var scms = {};
 		
 		return {
 			init: init,
-			fetch: fetch,
-			process: process
+			fetch: fetch
 		};
 
 		function fetch(url) {
@@ -18,6 +17,27 @@ var scms = {};
 			xhr.onload = load;
 			xhr.open("GET", url, true);
 			xhr.send();
+			history.pushState(null, null, url.substring(0, url.lastIndexOf(".")));
+		}
+
+		function generateTOC() {
+			//Get the h2's.
+			var panel = document.getElementById("scms-toc");
+			var panelString = '';
+			var theH2s = document.body.querySelectorAll("h2");
+			for (h = 0; h < theH2s.length; h++) {
+				theH2s[h].setAttribute("id","scms-tocAnchor" + h);
+				panelString += "<li><a href='#scms-tocAnchor" + h + "'>" + theH2s[h].innerHTML + "</a></li>";
+			}
+			
+			// Append.
+			if (panelString) {
+				panel.innerHTML = panelString;
+				document.getElementById("toc").style.display = 'block';
+			} else {
+				document.getElementById("toc").style.display = 'none';
+			}
+
 		}
 
 		function init() {
@@ -55,46 +75,42 @@ var scms = {};
 			if (!markdownEl) {
 				console.warn('No embedded Markdown found in this document.');
 				return;
+			} else {
+				var markdown = markdownEl.textContent || markdownEl.innerText;
 			}
 
-	var markdown = markdownEl.textContent || markdownEl.innerText;
-	
-	var newNode = document.createElement('div');
-	newNode.id = 'scms-markedUp';
-	newNode.className = 'markdown-body';
-	document.getElementById("content").appendChild(newNode);
-	
-	// Generate Markdown
-	var html = marked(markdown);
-	document.getElementById('scms-markedUp').innerHTML = html;
-	
-	// Style tables
-	var tableEls = document.getElementsByTagName('table');
-	for (var i=0, ii=tableEls.length; i<ii; i++) {
-		var tableEl = tableEls[i];
-		tableEl.className = 'table table-striped table-bordered';
-	}
+			//We (re)insert the target element to avoid CSS issues.
+			if (document.getElementById("scms-markedUp")) {
+				document.getElementById("content").removeChild(document.getElementById("scms-markedUp"));
+			}
+			
+			var newNode = document.createElement('div');
+			newNode.id = 'scms-markedUp';
+			newNode.className = 'markdown-body';
+			document.getElementById("content").appendChild(newNode);
 
-	//Generate TOC.
+			// Generate Markdown and write to target
+			var html = marked(markdown);
+			document.getElementById('scms-markedUp').innerHTML = html;
 
-	//Get the h2's.
-	var panel = document.getElementById("scms-toc");
-	var panelString = '';
-	var theH2s = document.body.querySelectorAll("h2");
-	for (h = 0; h < theH2s.length; h++) {
-		theH2s[h].setAttribute("id","scms-tocAnchor" + h);
-		panelString += "<li><a href='#scms-tocAnchor" + h + "'>" + theH2s[h].innerHTML + "</a></li>";
-	}
-	
-	// Append.
-	if (panelString) {
-		panel.innerHTML = panelString;
-	} else {
-		document.getElementById("toc").style.display = 'none';
-	}
+			relink();
+			style();
+			generateTOC();
+		}
 
-			// Style.
+		function relink() {
+			//Find any local links in the processed markdown and hijack them for ajax.
 
+			//...but how?
+		}
+		
+		function style() {
+			//Style tables
+			var tableEls = document.getElementsByTagName('table');
+			for (var i=0, ii=tableEls.length; i<ii; i++) {
+				var tableEl = tableEls[i];
+				tableEl.className = 'table table-striped table-bordered';
+			}
 		}
 		
 	})();
